8f6ca99791d5f272c0d68a4fb8f9edb9fd5626d1
eric-czech <eric.allen.czech@gmail.com>
2016-01-28 08:41:12 -0500

    Adding generic caching object

---
+++ functional/common/R/cache.R
@@ -0,0 +1,100 @@
+#'-----------------------------------------------------------------------------
+#' Data Caching Class
+#'
+#' This class is meant to be used as a convenience for organizing and retrieving
+#' large or epensive objects on disk.  Example usage:
+#' 
+#' basicConfig(level=loglevels['DEBUG']) # Cache messages all made @ DEBUG 
+#' cache <- Cache(dir='/tmp', project='myproject') # Objects stored at /tmp/myproject
+#' cache$invalidate('myobj') # Remove object from cache dir if it exists
+#' 
+#' # The following call will fetch the object by name if it already exists;
+#' # otherwise the "loader" function will be called and the result from that
+#' # function will be cached and returned
+#' d <- cache$load('myobj', loader=function(){data.frame(x=1:10)})
+#' 
+#' @author eczech
+#'-----------------------------------------------------------------------------
+library(logging)
+
+Cache <- setRefClass("Cache",
+  fields = list(dir = "character", project = "character"),
+  methods = list(
+    initialize = function(..., dir=file.path('~', '.Rcache'), project='default'){
+        callSuper(..., dir=dir, project=project)
+    },
+    normalizeKey = function(key){ paste0(key, '.Rdata') },
+    getPath = function(key) {
+      #' Returns a file path to the object in cache (rooted at dir/project)
+      
+      # Add extension to object key
+      key <- normalizeKey(key)
+      
+      # Create a path for the parent directory of the object
+      cpath <- file.path(dir, project)
+      
+      # Verify that the parent object already exists or that it could be created
+      if (!dir.exists(cpath))
+        dir.create(cpath, recursive=T)
+      if (!dir.exists(cpath))
+        stop(sprintf('Failed to create or validate cache directory "%s%', cpath))
+      
+      # Verify that the cached object file can be created if it does not 
+      # already exist (ie make sure there are no FS or permission errors)
+      cpath <- file.path(cpath, key)
+      if (!file.exists(cpath)){
+        valid <- file.create(cpath)
+        if (!valid) stop(sprintf('Unable to create cached object file at path "%s"', cpath))
+        if (!file.remove(cpath)) stop(sprintf('Failed to delete cache file "%s"', cpath))
+      }
+      
+      # Return the full path to the cached object
+      cpath
+    },
+    load = function(key, loader, compress=T){
+      #' Loads an object by the given name (ie key) if it exists or
+      #' creates this object using the given loader function and then
+      #' returns it (after saving it for later, faster retrieval)
+      
+      # Get the full path to the cached object
+      cpath <- getPath(key)
+      
+      # Load and save the object if it is not already cached
+      if (!file.exists(cpath)){
+        logdebug('Creating and storing cache object "%s"', cpath)
+        object <- loader()
+        base::save(object, file=cpath, compress=compress)
+      } else {
+        logdebug('Restoring cached object "%s" from disk', cpath)
+      }
+      
+      # Load and return the cached object
+      e <- new.env()
+      base::load(cpath, envir=e)
+      e$object
+    },
+    invalidate = function(key){
+      #' Deletes a file for the given key, if it exists in cache
+      cpath <- getPath(key)
+      logdebug('Invalidating cached object at path "%s"', cpath)
+      if (file.exists(cpath))
+        if (!file.remove(cpath)) 
+          stop(sprintf('Failed to remove cached object at path "%s"', cpath))
+    },
+    store = function(key, object, compress=T){
+      #' Stores the given object using the given name (key)
+      #' * Note that this should be used only to force a write of a 
+      #'   new cached object, which should be a rare use case
+      cpath <- getPath(key)
+      base::save(object, file=cpath, compress=compress)
+    }
+  )
+)
+
+# Example Usage:
+# basicConfig(level=loglevels['DEBUG'])
+# c <- Cache(dir='/tmp', project='test2')
+# key <- 'test'
+# c$invalidate(key)
+# d <- c$load('test', function(){data.frame(x=1:10)})
+# d <- c$load('test', function(){data.frame(x=1:10)})
