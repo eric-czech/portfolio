3bc209b2dfbc1dd1a39a4e5c9e1d6e89b6027017
Eric Czech <eric.allen.czech@gmail.com>
2016-05-12 16:27:01 -0500

    WMI research EDA

--- functional/ml/python/ml/model/summaries.py
+++ functional/ml/python/ml/model/summaries.py
@@ -17,13 +17,14 @@ def summarize_grid_parameters(res):
     return grid_params.groupby(['model_name', 'variable'])['value'].apply(lambda x: x.value_counts())
 
 
-def plot_model_scores(res, score_func=DEFAULT_REGRESSOR_SCORE, figsize=(12, 6), use_proba=None):
+def plot_model_scores(res, score_func=DEFAULT_REGRESSOR_SCORE, figsize=(12, 6), use_proba=None, **kwargs):
     scores = models.summarize_scores(res, score_func, use_proba=use_proba)
-    ax = scores .boxplot('score', 'model_name', figsize=figsize)
+    ax = scores.boxplot('score', 'model_name', figsize=figsize, **kwargs)
     return ax
 
 INTERPOLATE_FILL = lambda x: x.ffill().bfill()
 INTERPOLATE_LINEAR = lambda x: x.interpolate()
+
 
 def plot_curve(res, curve_func=roc_curve, interpolation=INTERPOLATE_LINEAR, plot_size=(8, 6)):
     d_roc = models.summarize_curve(res, curve_func=curve_func)
