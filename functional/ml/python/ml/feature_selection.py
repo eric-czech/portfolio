acde5a179a20c047dc3692a631368b9e5c2d1cc5
Eric Czech <eric.allen.czech@gmail.com>
2016-08-05 06:16:53 -0400

    feature selector

--- functional/ml/python/ml/feature_selection.py
+++ functional/ml/python/ml/feature_selection.py
@@ -1,5 +1,8 @@
 
 from sklearn.feature_selection import SelectKBest
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils import check_array
+
 import numpy as np
 
 
@@ -25,3 +28,70 @@ def get_static_feature_selector(all_features, target_features):
         return scores, np.abs(1 - scores)
 
     return SelectKBest(selector, k=np.sum(scores))
+
+
+def _identity(X):
+    """The identity function.
+    """
+    return X
+
+
+class FunctionTransformer(BaseEstimator, TransformerMixin):
+    """Constructs a transformer from an arbitrary callable.
+
+    ** This is class is from scikit-learn 17.1 and is included here for use in previous versions
+
+    A FunctionTransformer forwards its X (and optionally y) arguments to a
+    user-defined function or function object and returns the result of this
+    function. This is useful for stateless transformations such as taking the
+    log of frequencies, doing custom scaling, etc.
+
+    A FunctionTransformer will not do any checks on its function's output.
+
+    Note: If a lambda is used as the function, then the resulting
+    transformer will not be pickleable.
+
+    .. versionadded:: 0.17
+
+    Parameters
+    ----------
+    func : callable, optional default=None
+        The callable to use for the transformation. This will be passed
+        the same arguments as transform, with args and kwargs forwarded.
+        If func is None, then func will be the identity function.
+
+    validate : bool, optional default=True
+        Indicate that the input X array should be checked before calling
+        func. If validate is false, there will be no input validation.
+        If it is true, then X will be converted to a 2-dimensional NumPy
+        array or sparse matrix. If this conversion is not possible or X
+        contains NaN or infinity, an exception is raised.
+
+    accept_sparse : boolean, optional
+        Indicate that func accepts a sparse matrix as input. If validate is
+        False, this has no effect. Otherwise, if accept_sparse is false,
+        sparse matrix inputs will cause an exception to be raised.
+
+    pass_y: bool, optional default=False
+        Indicate that transform should forward the y argument to the
+        inner callable.
+
+    """
+    def __init__(self, func=None, validate=True,
+                 accept_sparse=False, pass_y=False):
+        self.func = func
+        self.validate = validate
+        self.accept_sparse = accept_sparse
+        self.pass_y = pass_y
+
+    def fit(self, X, y=None):
+        if self.validate:
+            check_array(X, self.accept_sparse)
+        return self
+
+    def transform(self, X, y=None):
+        if self.validate:
+            X = check_array(X, self.accept_sparse)
+        func = self.func if self.func is not None else _identity
+
+        return func(X, *((y,) if self.pass_y else ()))
