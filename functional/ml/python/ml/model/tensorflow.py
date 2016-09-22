67fce4ef76aa0457d6d93cd100333f9851261829
Eric Czech <eric.allen.czech@gmail.com>
2016-09-22 19:01:17 -0400

    tf classifier

--- functional/ml/python/ml/model/tensorflow.py
+++ functional/ml/python/ml/model/tensorflow.py
@@ -1,15 +1,19 @@
 
 import numpy as np
+import pandas as pd
 import tensorflow.contrib.learn.python.learn as learn
 from sklearn.base import BaseEstimator, ClassifierMixin
+import uuid
+import os
 
 
 class LearnClassifier(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, model_fn, model_params=None, model_config=None):
+    def __init__(self, model_fn, model_params=None, model_config=None, model_dir=None):
         self.model_fn = model_fn
         self.model_params = model_params
         self.model_config = model_config
+        self.model_dir = model_dir
 
     def fit(self, X, y, **kwargs):
         assert 'monitors' not in kwargs, \
@@ -22,7 +26,14 @@ class LearnClassifier(BaseEstimator, ClassifierMixin):
             self.monitors_ = kwargs['monitor_fn'](X, y)
             assert isinstance(self.monitors_, dict), 'Monitor map must be a dictionary'
             del kwargs['monitor_fn']
-        self.classifier_ = learn.Estimator(model_fn=self.model_fn, params=self.model_params, config=self.model_config)
+
+        model_dir = None
+        if self.model_dir is not None:
+            model_dir = os.path.join(self.model_dir, str(uuid.uuid4()))
+
+        self.classifier_ = learn.Estimator(
+                model_fn=self.model_fn, model_dir=model_dir,
+                params=self.model_params, config=self.model_config)
         self.classifier_.fit(X, y, monitors=list(self.monitors_.values()), **kwargs)
         return self
 
