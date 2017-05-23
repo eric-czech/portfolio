554a47705d18b35c83990b96fd7c3a4b8e1abeae
Eric Czech <eric.allen.czech@gmail.com>
2017-05-22 20:02:46 -0400

    tf utilities

--- functional/ml/python/ml/tensorflow/utilities.py
+++ functional/ml/python/ml/tensorflow/utilities.py
@@ -5,6 +5,43 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class TensorCollector(object):
+    """ Collector class to be used for retrieving individual tensor values"""
+
+    def __init__(self, enabled=True):
+        self.data = {}
+        self.enabled = enabled
+
+    def register(self, t, extraction_fn, name=None):
+        """
+        Register a tensor for collection
+        :param t: Tensor to collect data for
+        :param extraction_fn: Function with signature fn(x) returning any value, though this value will always
+            be appended to the end of a list on each training iteration so typically only a list, array or scalar
+            value is useful
+        :param name: Name to key tensor values by; if not given, tensor name will be used
+        :return: Original tensor (t)
+        """
+        if not self.enabled:
+            return t
+
+        if name is None:
+            n = t.name.split(':')[0]
+        else:
+            n = name
+
+        if n not in self.data:
+            self.data[n] = []
+
+        def extract_value(x):
+            self.data[n].append(extraction_fn(x))
+            return x
+        collect_op = tf.py_func(extract_value, [t], [t.dtype], name=n)[0]
+        with tf.control_dependencies([collect_op]):
+            r = tf.identity(t)
+        return r
+
+
 def tf_eval(exp):
     sess = tf.InteractiveSession()
     res = sess.run(exp)
